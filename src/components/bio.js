018bcf2ed3d470686edea3943afcf582d285ae05
Harold Peñaloza <hspe2507@gmail.com>
2021-05-26 23:32:54 -0500

    [ADD] something

--- src/components/bio.js
+++ src/components/bio.js
@@ -44,7 +44,7 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+          <strong>{author.name}</strong> {author?.summary || null}
           {` `}
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
             HarpeBlue
